d9f34c77d0292e90c324b90f5cb98d6616127c9e
avismm <amuhwerane@gmail.com>
2023-08-23 19:55:17 +0700

    Changes to HW3Arrays of class9

--- src/Class93DArrays/HW3Arrays.java
+++ src/Class93DArrays/HW3Arrays.java
@@ -4,10 +4,11 @@ public class HW3Arrays {
     public static void main(String[] args) {
 
         int[][] nums = {{1,2,3,},{4,5,6,},{7,8,9,}};
-
+        int sum=0;
         for (int[] array : nums) {
             for (int C : array)
-                System.out.print(C + " ");
+                sum=sum+C;
+                System.out.print(sum);
             //System.out.println(Arrays.toString(array));
 
         }
